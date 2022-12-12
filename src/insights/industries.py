b69ff844a914b3a0068419dfacfe099bb1e0fade
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-12-12 16:08:04 -0300

    exer3

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,14 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    data = read(path)
+    list_of_industry = set()
+    for row in data:
+        if row["industry"]:
+            list_of_industry.add(row["industry"])
+    return list_of_industry
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
