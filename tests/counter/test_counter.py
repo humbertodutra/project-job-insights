6e5782a8fffa0a4ec06b68cd3b4a71b82def4874
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-12-12 18:55:09 -0300

    test counter, exer 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,18 @@
+import pytest 
+from src.pre_built.counter import count_ocurrences
+
 # from src.pre_built.counter import count_ocurrences
+
+path = "tests/mocks/jobs.csv"
 
 
 def test_counter():
-    pass
+    assert count_ocurrences(path, "developer") == 3
+    assert count_ocurrences(path, "DEVELOPER") == 3
+
+    with pytest.raises(TypeError):
+        count_ocurrences(path)
+        count_ocurrences("developer")
+
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("jobs_not_found.csv", "developer")
