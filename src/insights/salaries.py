152d85cfa03675d475a318b01ce4b6519aa7a9e5
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-12-12 16:25:59 -0300

    exer 4,5(min, max)

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,40 +1,23 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    data = read(path)
+    max_salary = []
+    for row in data:
+        if row["max_salary"] and row["max_salary"].isdigit():
+            max_salary.append(int(row["max_salary"]))
+    return max(max_salary)
 
 
 def get_min_salary(path: str) -> int:
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    data = read(path)
+    min_salary = []
+    for row in data:
+        if row["min_salary"] and row["min_salary"].isdigit():
+            min_salary.append(int(row["min_salary"]))
+    return min(min_salary)
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
